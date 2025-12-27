5579bd1047e384ed5662100a8ef522c8cf168741
Omay238 <238maculo238@gmail.com>
2025-12-27 04:14:20 -0700

    split arch-chroot

---
+++ part1.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+cd etc
+
+find . -type f | while IFS= read -r path; do
+  rel="${path#./}"
+  target="/mnt/etc/$rel"
+
+  mkdir -p "$(dirname "$target")"
+
+  echo "copying $PWD/$rel to $target"
+  cp "$PWD/$rel" "$target"
+done
+
+echo "what keymap are you using? type 'us' for a standard qwerty keyboard"
+read -p "> " keymap
+echo "KEYMAP=$keymap" > /mnt/etc/vconsole.conf
+
+pacstrap -K /mnt base-devel linux linux-firmware neovim git
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cp part2.sh /mnt
+
+echo "now, run arch-chroot /mnt"
+echo "then, /part2.sh"
---
+++ part2.sh
@@ -0,0 +1,55 @@
+echo "what is your region?"
+ls /usr/share/zoneinfo
+read -p "> " region
+
+if [ -f "/usr/share/zoneinfo/$region" ]; then
+  ln -s "/usr/share/zoneinfo/$region" /etc/localtime
+else
+  echo "what is your time zone?"
+  ls "/usr/share/zoneinfo/$region"
+  read -p "> " zone
+  ln -s "/usr/share/zoneinfo/$region/$zone" /etc/localtime
+fi
+
+hwclock --systohc
+
+locale-gen
+echo "you're going into locale.gen. please uncomment whichever UTF-8 locales you will be using"
+sleep 3
+nvim /etc/locale.gen
+echo "type whichever your primary language zone is (e.g. en_US.UTF-8)"
+read -p "> " lang
+echo "LANG=$lang" > /etc/locale.conf
+
+echo "now, for your hostname"
+read -p "> " hostname
+echo "$hostname" > /etc/hostname
+
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+echo "enter root password"
+passwd
+
+echo "finally, what do you want your username to be?"
+read -p "> " username
+useradd "$username"
+usermod -G wheel "$username"
+mkdir "/home/$username"
+chown "$username" "/home/$username"
+
+su "$username"
+cd ~
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ..
+
+git clone https://github.com/Omay238/owomay-dotfiles
+cd owomay-dotfiles
+./update.sh
+exit
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+mkinitcpio -P
+
+rm /part2.sh
